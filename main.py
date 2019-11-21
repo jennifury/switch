09be8738f4964dcef1183e4d9d557048c61ed164
Jennifer Yang <jennifury25@gmail.com>
2019-11-20 20:05:07 -0800

    updated the handler

--- main.py
+++ main.py
@@ -17,9 +17,43 @@ class AboutUsHandler(webapp2.RequestHandler):
         template = jinja_env.get_template('templates/about_us.html')
         self.response.write(template.render())
 
+class ContactUsHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/contact_us.html')
+        self.response.write(template.render())
 
+class SellingHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/selling.html')
+        self.response.write(template.render())
+
+class DonateHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/donate.html')
+        self.response.write(template.render())
+
+class FreelanceHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/freelance.html')
+        self.response.write(template.render())
+
+class TradingHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/trading.html')
+        self.response.write(template.render())
+
+class WishlistHandler(webapp2.RequestHandler):
+    def get(self):
+        template = jinja_env.get_template('templates/wishlist.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', HomepageHandler),
-    ('/about_us', AboutUsHandler)
+    ('/about_us', AboutUsHandler),
+    ('/contact_us', ContactUsHandler),
+    ('/selling', SellingHandler),
+    ('/donate', DonateHandler),
+    ('/freelance', FreelanceHandler),
+    ('/trade', TradingHandler),
+    ('/wishlist', WishlistHandler),
 ], debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -1,17 +1,49 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+var slideIndex = 0;
+showSlides();
+var slides,dots;
 
-// Get the header
-var header = document.getElementById("switchHeader");
+// Next/previous controls
+function plusSlides(position) {
+    slideIndex +=position;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    else if(slideIndex<1){slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+// Thumbnail image controls
+function currentSlide(index) {
+    if (index> slides.length) {index = 1}
+    else if(index<1){index = slides.length}
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[index-1].style.display = "block";
+    dots[index-1].className += " active";
+}
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+function showSlides() {
+    var i;
+    slides = document.getElementsByClassName("mySlides");
+    dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
