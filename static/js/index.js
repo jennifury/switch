9697a01672342874489c27545cfdeaebd00a23f1
Jennifer Yang <jennifury25@gmail.com>
2019-11-30 01:47:55 -0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+
+
 var slideIndex = 0;
 showSlides();
 var slides,dots;
@@ -48,59 +49,7 @@ function showSlides() {
     dots[slideIndex-1].className += " active";
     setTimeout(showSlides, 5000); // Change image every 3 seconds
 }
-<<<<<<< HEAD
-=======
-var slideIndex = 0;
-showSlides();
-var slides,dots;
 
-// Next/previous controls
-function plusSlides(position) {
-    slideIndex +=position;
-    if (slideIndex> slides.length) {slideIndex = 1}
-    else if(slideIndex<1){slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-}
-
-// Thumbnail image controls
-function currentSlide(index) {
-    if (index> slides.length) {index = 1}
-    else if(index<1){index = slides.length}
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[index-1].style.display = "block";  
-    dots[index-1].className += " active";
-}
-
-function showSlides() {
-    var i;
-    slides = document.getElementsByClassName("mySlides");
-    dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex> slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 3 seconds
-}
->>>>>>> 45822f95c64ba405a095d8755eb5298f8b636532
-=======
 
 //createpost
 function onSelection() {
@@ -112,4 +61,3 @@ function onSelection() {
         document.getElementsByName('price')[0].value='';
     }
 }
->>>>>>> 46dce21fa276b9c566c34f395ef515394e86d5aa
