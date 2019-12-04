dc8a98199087a025b04f205afab89d20f1b9f10f
Jennifer Yang <jennifury25@gmail.com>
2019-12-04 15:31:20 -0800

    update code

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
+from google.appengine.ext import db
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -9,48 +11,124 @@ jinja_env = jinja2.Environment(
 
 class HomepageHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/homepage.html')
-        self.response.write(template.render())
+        home_template = jinja_env.get_template('templates/homepage.html')
+        self.response.write(home_template.render())
 
 class AboutUsHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/about_us.html')
-        self.response.write(template.render())
+        about_template = jinja_env.get_template('templates/about_us.html')
+        self.response.write(about_template.render())
 
 class ContactUsHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/contact_us.html')
-        self.response.write(template.render())
+        contact_template = jinja_env.get_template('templates/contact_us.html')
+        self.response.write(contact_template.render())
 
 class SellingHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/selling.html')
-        self.response.write(template.render())
+        proceed = "selling"
+        posts = NewPost.query().filter(NewPost.category == proceed).fetch()
+
+        list_dict = {
+        "num_of_items": len(posts),
+        "items": posts
+        }
+        sell_template = jinja_env.get_template('templates/selling.html')
+        self.response.write(sell_template.render(list_dict))
 
 class DonateHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/donating.html')
-        self.response.write(template.render())
+        proceed = "donating"
+        posts = NewPost.query().filter(NewPost.category == proceed).fetch()
+
+        list_dict = {
+        "num_of_items": len(posts),
+        "items": posts
+        }
+        donate_template = jinja_env.get_template('templates/donating.html')
+        self.response.write(donate_template.render(list_dict))
 
 class FreelanceHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/freelance.html')
-        self.response.write(template.render())
+        proceed = "freelance"
+        posts = NewPost.query().filter(NewPost.category == proceed).fetch()
+
+        list_dict = {
+        "num_of_items": len(posts),
+        "items": posts
+        }
+        free_template = jinja_env.get_template('templates/freelance.html')
+        self.response.write(free_template.render(list_dict))
 
 class TradingHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/trading.html')
-        self.response.write(template.render())
+        proceed = "trading"
+        posts = NewPost.query().filter(NewPost.category == proceed).fetch()
+
+        list_dict = {
+        "num_of_items": len(posts),
+        "items": posts
+        }
+
+
+        trade_template = jinja_env.get_template('templates/trading.html')
+        self.response.write(trade_template.render(list_dict))
 
 class WishlistHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/wishlist.html')
-        self.response.write(template.render())
+        wish_template = jinja_env.get_template('templates/wishlist.html')
+        self.response.write(wish_template.render())
+
+class NewPost(ndb.Model):
+    image = ndb.BlobProperty()
+    category = ndb.StringProperty(required=True)
+    subcategory = ndb.StringProperty(required=True)
+    title = ndb.StringProperty(required=True)
+    price = ndb.StringProperty(required=True)
+    description = ndb.StringProperty(required=True)
+    communication = ndb.StringProperty(required=True)
 
 class CreatePostHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_env.get_template('templates/create_post.html')
-        self.response.write(template.render())
+        post_template = jinja_env.get_template('templates/create_post.html')
+        self.response.write(post_template.render())
+
+    def post(self):
+        post_cat = self.request.get('catego ry')
+        post_scat = self.request.get('subcategory')
+        post_title = self.request.get('title')
+        post_price = self.request.get('price')
+        post_comm = self.request.get('communication')
+        post_pics = self.request.get('pics')
+        # file_upload = self.request.POST.get("pics", None)
+        post_desc = self.request.get('description')
+
+        # new_name = NewPost(name=poster_name, item = post_item, paragraph = post_desc, image= file_upload.file.read())
+        new_name = NewPost(category=poster_cat, subcategory = post_scat, paragraph = post_desc,
+                            price = post_price, communication = post_comm,image= db.Blob(str(post_pics)))
+
+        new_name.put()
+        self.redirect("/thankyou")
+
+class ImageHandler(webapp2.RequestHandler):
+    def get(self):
+        posts = NewPost.query().fetch()
+
+        list_dict = {
+        "num_of_items": len(posts)
+        }
+        index = self.request.get('index')
+        index = int(index)
+        if posts[index].image:
+          self.response.headers['Content-Type'] = "image/jpg"
+          self.response.out.write(posts[index].image)
+        else:
+          self.redirect('/static/img/noimage.svg')
+
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        thanks_template = jinja_env.get_template('templates/thank_you.html')
+        self.response.write(thanks_template.render())
 
 app = webapp2.WSGIApplication([
     ('/', HomepageHandler),
@@ -61,5 +139,6 @@ app = webapp2.WSGIApplication([
     ('/freelance', FreelanceHandler),
     ('/trade', TradingHandler),
     ('/wishlist', WishlistHandler),
+    ('/thankyou', ThanksHandler),
     ('/create_post', CreatePostHandler),
 ], debug=True)
